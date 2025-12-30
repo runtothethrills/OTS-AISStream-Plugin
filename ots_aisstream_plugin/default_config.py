db4bb896441739f477131659820c7352d30c3685
runtothethrills <contactrttt@gmail.com>
2025-12-29 19:24:14 -0500

    Update default_config.py

--- ots_aisstream_plugin/default_config.py
+++ ots_aisstream_plugin/default_config.py
@@ -12,7 +12,7 @@ class DefaultConfig:
     # Config options go here in all caps with the name of your plugin first
     # This file will be loaded first, followed by user overrides from config.yml
     OTS_AISSTREAM_PLUGIN_ENABLED = True
-    OTS_AISSTREAM_PLUGIN_API_KEY = "your_api_key"
+    OTS_AISSTREAM_PLUGIN_API_KEY = "6e4896e6034e0ee7ebccb7bab5bce7fbac4df994"
     OTS_AISSTREAM_PLUGIN_BBOX = [[[25.835302, -80.207729], [25.602700, -79.879297]], [[33.772292, -118.356139], [33.673490, -118.095731]] ]
     OTS_AISSTREAM_PLUGIN_COT_TYPE = "a-u-S-X-M"
     OTS_AISSTREAM_PLUGIN_COT_STALE_TIME = 3600  # CoT stale time in seconds
